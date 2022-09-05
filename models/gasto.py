f848091c6981e6f138285e33879d96b74d67156e
Leandro Ferrari <111808524+Leandro090685@users.noreply.github.com>
2022-09-05 19:05:38 -0300

    Add files via upload

---
+++ models/gasto.py
@@ -0,0 +1,10 @@
+
+
+class Gasto:
+    def __init__(self, fecha:str,rubro:str,descripcion:str,importe:float) -> None:
+        self.fecha = fecha
+        self.rubro = rubro
+        self.descripcion = descripcion
+        self.importe = importe
+        
+    
---
+++ models/rubro.py
@@ -0,0 +1,5 @@
+
+class Rubro:
+    def __init__(self, name:str) -> None:
+        self.name = name
+        
